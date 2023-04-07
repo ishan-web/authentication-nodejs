b35e0767326f0d2a70d4b692fb93a183d51b039a
Your Name <ishanpoudel58@gmail.com>
2023-04-07 22:16:26 +0545

    nav

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const db = require("./routes/db-config");   
+const db = require("./routes/db-config");     //calling 'const db' from 'db-config routes' which is used to connect to the database
 const app = express();
 const cookie = require("cookie-parser");
-app.use("/js", express.static(--dirname + "./public/js"))
-app.use("/css", express.static(__dirname + "./public/css"))
-app.set("view engine", "ejs");
-app.set("views", "./views");
+const PORT = process.env.PORT || 5000;   //calling the port 
+
+app.use("/js", express.static(__dirname + "./public/js")) //giving path to read js files inside public
+app.use("/css", express.static(__dirname + "./public/css")) //path for css file inside public folder
+app.set("view engine", "ejs");  // this is make server to render any ejs file mentioned within the path  mentioned below
+app.set("views", "./views");   //giving the path to ejs file
+app.use(cookie());
+app.use(express.json());
+db.connect((err) =>{
+    if (err)  throw err;   //throw err if there is problem with the connection with the database
+})
+
+app.use("/", require("./routes/pages"));
+// app.use("/api", register("./controllers/auth"));
+app.listen(PORT);
 
