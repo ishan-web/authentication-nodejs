057cab476c6391916430f0909830c987e5893945
Your Name <ishanpoudel58@gmail.com>
2023-04-07 22:17:04 +0545

    nav.

--- routes/db-config.js
+++ routes/db-config.js
@@ -1,4 +1,5 @@
 const sql = require('mysql');
+const dotenv = require("dotenv").config();
 const db = sql.createConnection({
     host: process.env.DATABASE_HOST,
     user:process.env.DATABASE_USER,
